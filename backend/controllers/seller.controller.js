c496d501a03a778243d87c18c0bfef03627d0fef
Shivam Mandal <sanchayetamandal83404@gmail.com>
2025-12-28 15:59:36 +0530

    modified import in seller controller

--- backend/controllers/seller.controller.js
+++ backend/controllers/seller.controller.js
@@ -1,6 +1,6 @@
 
 import Company from '../models/seller.model.js';
-import User from '../models/user.mode
+import User from '../models/user.model.js';
 
 
 export const upsertCompany = async (req, res) => {
