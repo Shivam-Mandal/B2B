1c4e75d13b95cce010579fddbe31de76bacf5fc7
Shivam Mandal <sanchayetamandal83404@gmail.com>
2025-12-30 23:21:41 +0530

    added hosted url

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://b2b-frontend-vjb4.onrender.com'],
     credentials: true,
   })
 );
